6d17e711ba54efd0430c9b4da96e5022e9d4c19e
Yann Dubois <yanndubois96@gmail.com>
2022-02-23 03:31:28 -0800

    [ENH] add slfdstl.

--- bin/simclr_remote.sh
+++ bin/simclr_remote.sh
@@ -7,10 +7,11 @@
     config.DISTRIBUTED.NUM_PROC_PER_NODE=1 \
     config.SLURM.PARTITION=interactive \
     config.SLURM.ADDITIONAL_PARAMETERS.qos=nopreemption \
+    config.SLURM.TIME_HOURS=0 \
     config.SLURM.TIME_MINUTES=5 \
     config.SLURM.MEM_GB=16 \
     config.DATA.TRAIN.DATA_LIMIT=10000
-    #config.LOG_FREQUENCY=50 \
-    #config.OPTIMIZER.num_epochs=7 \
+    config.LOG_FREQUENCY=50 \
+    config.OPTIMIZER.num_epochs=7 \
 
 
