bcaa147c942773e06b0d1112147b084916f5f630
Yann Dubois <yanndubois96@gmail.com>
2022-03-29 14:01:24 -0700

    [BUG] dissl_reg working

---
+++ bin/dstlreg_remote.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+./dev/launch_slurm.sh \
+    dstlreg_dir \
+    config=pretrain/dstl/dstl_reg_resnet \
+    +config/server=remote \
+    config.DISTRIBUTED.NUM_NODES=1 \
+    config.DISTRIBUTED.NUM_PROC_PER_NODE=8 \
+    config.SLURM.MEM_GB=128 \
+    config.SLURM.NUM_CPU_PER_PROC=4 \
+#    config.SLURM.PARTITION=interactive \
+#    config.SLURM.ADDITIONAL_PARAMETERS.qos=nopreemption \
+#    config.SLURM.TIME_HOURS=0 \
+#    config.SLURM.TIME_MINUTES=5 \
+#    config.SLURM.MEM_GB=16 \
+#    config.DATA.TRAIN.DATA_LIMIT=10000
+#    config.LOG_FREQUENCY=50 \
+#    config.OPTIMIZER.num_epochs=7 \
+
+
