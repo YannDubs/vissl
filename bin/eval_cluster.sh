f0c504ff5a81e7784256c0f94f2f64824b524001
Yann Dubois <yanndubois96@gmail.com>
2022-03-28 19:54:19 -0700

    [ENH] add eval_cluster

--- bin/eval_cluster.sh
+++ bin/eval_cluster.sh
@@ -3,6 +3,8 @@
 dir=$1
 mkdir "$dir"/eval_logs
 
+sbatch <<EOT
+#!/usr/bin/env bash
 #SBATCH --job-name=eval
 #SBATCH --partition=p100,t4v1,t4v2,rtx6000
 #SBATCH --gres=gpu:1
@@ -17,15 +19,16 @@ source ~/.bashrc
 
 # EXTRACT FEATURES
 conda activate vissl
-bin/extract_features.sh $dir
+bin/extract_features.sh "$dir"
 
 # LINEAR EVAL
 conda activate probing
-python tools/linear_eval.py --feature-path $dir/features --out-path $dir/eval --is-no-progress-bar
-python tools/linear_eval.py --feature-path $dir/features --out-path $dir/evalw3l01 --weight-decay 3e-6 --lr 0.1 --is-no-progress-bar
-python tools/linear_eval.py --feature-path $dir/features --out-path $dir/evalw5l01 --weight-decay 5e-6 --lr 0.1 --is-no-progress-bar
-python tools/linear_eval.py --feature-path $dir/features --out-path $dir/evalw7l01 --weight-decay 7e-6 --lr 0.1 --is-no-progress-bar
-python tools/linear_eval.py --feature-path $dir/features --out-path $dir/evalw1l01 --weight-decay 1e-6 --lr 0.1 --is-no-progress-bar
-python tools/linear_eval.py --feature-path $dir/features --out-path $dir/evalw3l03 --weight-decay 3e-6 --lr 0.3 --is-no-progress-bar
-python tools/linear_eval.py --feature-path $dir/features --out-path $dir/evalw3l005 --weight-decay 3e-6 --lr 0.05 --is-no-progress-bar
-python tools/linear_eval.py --feature-path $dir/features --out-path $dir/evalw3l05 --weight-decay 3e-6 --lr 0.5 --is-no-progress-bar
+python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/eval --is-no-progress-bar
+python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/evalw3l01 --weight-decay 3e-6 --lr 0.1 --is-no-progress-bar
+python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/evalw5l01 --weight-decay 5e-6 --lr 0.1 --is-no-progress-bar
+python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/evalw7l01 --weight-decay 7e-6 --lr 0.1 --is-no-progress-bar
+python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/evalw1l01 --weight-decay 1e-6 --lr 0.1 --is-no-progress-bar
+python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/evalw3l03 --weight-decay 3e-6 --lr 0.3 --is-no-progress-bar
+python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/evalw3l005 --weight-decay 3e-6 --lr 0.05 --is-no-progress-bar
+python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/evalw3l05 --weight-decay 3e-6 --lr 0.5 --is-no-progress-bar
+EOT
