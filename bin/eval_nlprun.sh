0d50dfbd0156b06c0a3bea85dd6e54df7e55401c
Yann Dubois <yanndubois96@gmail.com>
2022-05-03 22:12:31 -0700

    [ENH] add large running on remote.

--- bin/eval_nlprun.sh
+++ bin/eval_nlprun.sh
@@ -26,6 +26,13 @@ source ~/.zshrc
 
 # LINEAR EVAL
 conda activate probing
+python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/eval_w1e-6_l01_b4096 --weight-decay 1e-6 --lr 0.1 --batch-size 4096 --is-no-progress-bar --is-monitor-test
+python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/eval_w3e-6_l01_b4096 --weight-decay 3e-6 --lr 0.1 --batch-size 4096 --is-no-progress-bar --is-monitor-test
+python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/eval_w1e-6_l01_b4096_e300 --weight-decay 1e-6 --lr 0.1 --batch-size 4096 --n-epochs 300 --is-no-progress-bar --is-monitor-test
+python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/eval_w1e-5_l01_bn_4096 --weight-decay 1e-5 --lr 0.1 --is-batchnorm --batch-size 4096 --is-no-progress-bar --is-monitor-test
+python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/eval_w1e-4_l01_bn_4096 --weight-decay 1e-4 --lr 0.1 --is-batchnorm --batch-size 4096 --is-no-progress-bar --is-monitor-test
+python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/eval_w1e-4_l01_bn_4096_e300  --weight-decay 1e-4 --lr 0.1 --is-batchnorm --n-epochs 300 --batch-size 4096 --is-no-progress-bar --is-monitor-test
+python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/eval_w1e-4_l03_bn_4096_e300  --weight-decay 1e-4 --lr 0.3 --is-batchnorm --n-epochs 300 --batch-size 4096 --is-no-progress-bar --is-monitor-test
 python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/eval --is-no-progress-bar --is-monitor-test
 python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/eval_w1e-6_l01_b2048 --weight-decay 1e-6 --lr 0.1 --batch-size 2048 --is-no-progress-bar --is-monitor-test
 python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/eval_w1e-5_l01_b2048 --weight-decay 1e-5 --lr 0.1 --batch-size 2048 --is-no-progress-bar --is-monitor-test
@@ -39,5 +46,4 @@ python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/eva
 python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/eval_w1e-4_l03_bn_b2048 --weight-decay 1e-4 --lr 0.3 --is-batchnorm --batch-size 2048 --is-no-progress-bar --is-monitor-test
 python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/eval_w1e-5_l03_b2048_e300 --weight-decay 1e-5 --lr 0.3 --batch-size 2048 --n-epochs 300 --is-no-progress-bar --is-monitor-test
 python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/eval_w1e-4_l03_b2048_e300 --weight-decay 1e-4 --lr 0.3 --batch-size 2048 --n-epochs 300 --is-no-progress-bar --is-monitor-test
-python tools/linear_eval.py --feature-path "$dir"/features --out-path "$dir"/eval_w1e-6_l01_b4096 --weight-decay 1e-6 --lr 0.1 --batch-size 4096 --is-no-progress-bar --is-monitor-test
 EOT
---
+++ bin_final/swav_queue_sphinx.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+./dev/launch_slurm.sh \
+    swav_queuelong_dir \
+    config=pretrain/swav/swav_queuelong \
+    +config/server=sphinx1 \
+    config.DISTRIBUTED.NUM_NODES=1 \
+    config.DISTRIBUTED.NUM_PROC_PER_NODE=8 \
+    config.DATA.TRAIN.DATA_PATHS=["./data/imagenet/train"] \
+    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=384 \
+    config.DATA.NUM_DATALOADER_WORKERS=31 \
+    config.SLURM.PORT_ID=40061 \
+    config.SLURM.NAME=swav_queuelong \
+    config.SLURM.MEM_GB=1000 \
+    config.SLURM.NUM_CPU_PER_PROC=32 \
