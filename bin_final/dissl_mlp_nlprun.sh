409ffd5230167d61a4ead433dc8c0bab91257866
Yann Dubois <yanndubois96@gmail.com>
2022-05-04 09:08:13 -0700

    [ENH] add dissl_mlp_remote

--- bin_final/dissl_mlp_nlprun.sh
+++ bin_final/dissl_mlp_nlprun.sh
@@ -3,14 +3,13 @@
 ./dev/launch_slurm.sh \
     dissl_mlp_dir \
     config=pretrain/dstl/dissl_mlp \
-    +config/server=remote_large \
-    config.DISTRIBUTED.NUM_NODES=2 \
+    +config/server=nlp_XL \
+    config.DISTRIBUTED.NUM_NODES=1 \
     config.DISTRIBUTED.NUM_PROC_PER_NODE=4 \
-    config.DATA.TRAIN.DATA_PATHS=["./data/imagenet256/train"] \
+    config.DATA.TRAIN.DATA_PATHS=["./data/biggest/imagenet/train"] \
     config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=196 \
-    config.DATA.NUM_DATALOADER_WORKERS=9 \
+    config.DATA.NUM_DATALOADER_WORKERS=11 \
     config.SLURM.PORT_ID=40090 \
     config.SLURM.NAME=dissl_mlp \
-    config.SLURM.MEM_GB=166 \
-    config.SLURM.NUM_CPU_PER_PROC=10 \
-    config.SLURM.TIME_HOURS=120 \
+    config.SLURM.MEM_GB=110 \
+    config.SLURM.NUM_CPU_PER_PROC=12 \
