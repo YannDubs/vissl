a76eb6b61677bc1b451b67a8ee2fe26adbf9add8
Yann Dubois <yanndubois96@gmail.com>
2022-05-10 17:09:32 -0700

    [ENH] large lr

--- bin_final/augevallr01_dissl_res_remote.sh
+++ bin_final/augevallr01_dissl_res_remote.sh
@@ -4,7 +4,7 @@ model_name=dissl_res
 base_dir="$model_name"_dir
 
 ./dev/launch_slurm.sh \
-    $base_dir/augeval \
+    $base_dir/augeval_l01 \
     config=benchmark/linear_image_classification/imagenet1k/eval_resnet_in1k_linear_largelr \
     +config/server=remote_large \
     config.DISTRIBUTED.NUM_NODES=1 \
@@ -14,7 +14,7 @@ base_dir="$model_name"_dir
     config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=196 \
     config.DATA.NUM_DATALOADER_WORKERS=9 \
     config.SLURM.PORT_ID=40090 \
-    config.SLURM.NAME=augeval_"$model_name" \
+    config.SLURM.NAME=augeval_l01_"$model_name" \
     config.SLURM.MEM_GB=166 \
     config.SLURM.NUM_CPU_PER_PROC=10 \
     config.SLURM.TIME_HOURS=120 \
