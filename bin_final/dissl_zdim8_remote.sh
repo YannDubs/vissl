56f14cfd042bbca6d24bf935dc4e77f7c2f0c930
Yann Dubois <yanndubois96@gmail.com>
2022-05-11 01:04:17 -0700

    [BIG] add zdim and zskip

---
+++ bin_final/dissl_zdim8_remote.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+./dev/launch_slurm.sh \
+    dissl_zdim8_dir \
+    config=pretrain/dstl/dissl_zdim8 \
+    +config/server=sphinx1 \
+    config.DISTRIBUTED.NUM_NODES=2 \
+    config.DISTRIBUTED.NUM_PROC_PER_NODE=4 \
+    config.DATA.TRAIN.DATA_PATHS=["./data/imagenet/train"] \
+    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=196 \
+    config.DATA.NUM_DATALOADER_WORKERS=9 \
+    config.SLURM.PORT_ID=40046 \
+    config.SLURM.NAME=dissl_zdim8 \
+    config.SLURM.MEM_GB=166 \
+    config.SLURM.NUM_CPU_PER_PROC=10 \
+    config.SLURM.TIME_HOURS=120
