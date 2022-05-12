96bd6cad7b317823ea64e90ca8325be691cae115
Yann Dubois <yanndubois96@gmail.com>
2022-05-12 03:37:23 -0700

    [ENH] add all the nomulti.

---
+++ bin/test_cissl_remote_noslurm.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+python3 tools/run_distributed_engines.py \
+    hydra.verbose=true \
+    config=pretrain/cntr/cissl_nomulti \
+    config.CHECKPOINT.DIR="./test_simclr/checkpoints" \
+    config.DISTRIBUTED.NUM_NODES=1 \
+    config.DISTRIBUTED.NUM_PROC_PER_NODE=1 \
+    config.SLURM.USE_SLURM=False \
+    config.DATA.TRAIN.DATA_LIMIT=100000 \
+    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=96 \
+    config.OPTIMIZER.num_epochs=3 
