1513984c5c67b9fd77cb012f01d273ae2a705904
Yann Dubois <yanndubois96@gmail.com>
2022-05-16 15:56:21 -0700

    [ENH] moco optim

---
+++ bin_final/moco_nomulti_optim_sphinx.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+model=moco_nomulti_optim
+
+./dev/launch_slurm.sh \
+    "$model"_dir \
+    config=pretrain/moco/"$model" \
+    +config/server=sphinx1 \
+    config.DISTRIBUTED.NUM_NODES=1 \
+    config.DISTRIBUTED.NUM_PROC_PER_NODE=8 \
+    config.DATA.TRAIN.DATA_PATHS=["./data/imagenet/train"] \
+    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=128 \
+    config.DATA.NUM_DATALOADER_WORKERS=31 \
+    config.SLURM.PORT_ID=40020 \
+    config.SLURM.NAME="$model" \
+    config.SLURM.MEM_GB=1000 \
+    config.SLURM.NUM_CPU_PER_PROC=32 \
