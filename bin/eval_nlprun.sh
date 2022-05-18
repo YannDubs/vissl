2b2d3c501f9adf5754b535728ddd240bfd8f434d
Yann Dubois <yanndubois96@gmail.com>
2022-05-18 11:55:47 -0700

    [ENH] eval zdim long

--- bin/eval_nlprun.sh
+++ bin/eval_nlprun.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 #SBATCH --exclude=jagupard10,jagupard11,jagupard12,jagupard13,jagupard14,jagupard17,jagupard20,jagupard30,jagupard31
+#SBATCH --nodelist=jagupard29
 dir="$1"
 sffx="$2"
 mkdir -p "$dir"/eval_logs
@@ -15,7 +16,7 @@ sbatch <<EOT
 #SBATCH --qos=normal
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
-#SBATCH --nodelist=jagupard29
+#SBATCH --exclude=jagupard10,jagupard11,jagupard12,jagupard13,jagupard14,jagupard15,jagupard16,jagupard17,jagupard18,jagupard20,jagupard30,jagupard31
 #SBATCH --output="$dir"/eval_logs/slurm-%j.out
 #SBATCH --error="$dir"/eval_logs/slurm-%j.err
 
