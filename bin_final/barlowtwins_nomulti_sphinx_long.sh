78be8cb93e54fe625e63bb10c3f93770d01a2bc8
Yann Dubois <yanndubois96@gmail.com>
2022-05-19 23:57:54 -0700

    [ENH] all long

---
+++ bin_final/barlowtwins_nomulti_sphinx_long.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+model=barlowtwins_nomulti_long
+
+./dev/launch_slurm.sh \
+    "$model"_dir \
+    config=pretrain/barlow_twins/"$model" \
+    +config/server=sphinx1_std \
+    config.DISTRIBUTED.NUM_NODES=1 \
+    config.DISTRIBUTED.NUM_PROC_PER_NODE=8 \
+    config.DATA.TRAIN.DATA_PATHS=["./data/imagenet/train"] \
+    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=320 \
+    config.DATA.NUM_DATALOADER_WORKERS=31 \
+    config.SLURM.PORT_ID=40021 \
+    config.SLURM.NAME="$model" \
+    config.SLURM.MEM_GB=1000 \
+    config.SLURM.NUM_CPU_PER_PROC=32 \
