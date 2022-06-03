834585b520aece5a7f6d258c614e3c1b1118dc82
Yann Dubois <yanndubois96@gmail.com>
2022-06-02 22:42:33 -0700

    [CONF] warmbt noema and cos

--- bin_final/dissl_zdim8_nomulti_noema_cos_warmbt_sphinx.sh
+++ bin_final/dissl_zdim8_nomulti_noema_cos_warmbt_sphinx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-model=dissl_zdim8_nomulti_noema2_warmb
+model=dissl_zdim8_nomulti_noema_cos_warmbt
 
 ./dev/launch_slurm.sh \
     "$model"_dir \
@@ -11,7 +11,7 @@ model=dissl_zdim8_nomulti_noema2_warmb
     config.DATA.TRAIN.DATA_PATHS=["./data/imagenet/train"] \
     config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=320 \
     config.DATA.NUM_DATALOADER_WORKERS=31 \
-    config.SLURM.PORT_ID=40032 \
+    config.SLURM.PORT_ID=40036 \
     config.SLURM.NAME="$model" \
     config.SLURM.MEM_GB=1000 \
     config.SLURM.NUM_CPU_PER_PROC=32 \
--- bin_final/dissl_zdim8_nomulti_noema_warmbt_sphinx.sh
+++ bin_final/dissl_zdim8_nomulti_noema_warmbt_sphinx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-model=dissl_zdim8_nomulti_noema_cos_warmt
+model=dissl_zdim8_nomulti_noema_warmbt
 
 ./dev/launch_slurm.sh \
     "$model"_dir \
@@ -11,7 +11,7 @@ model=dissl_zdim8_nomulti_noema_cos_warmt
     config.DATA.TRAIN.DATA_PATHS=["./data/imagenet/train"] \
     config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=320 \
     config.DATA.NUM_DATALOADER_WORKERS=31 \
-    config.SLURM.PORT_ID=40036 \
+    config.SLURM.PORT_ID=40044 \
     config.SLURM.NAME="$model" \
     config.SLURM.MEM_GB=1000 \
     config.SLURM.NUM_CPU_PER_PROC=32 \
