792d794b67c850ef7fd49e3e7bb53f8dc17eaa82
Yann Dubois <yanndubois96@gmail.com>
2022-08-02 20:10:54 -0700

    [ENG] add swav caltech

---
+++ bin_final/trnsfeval_swav_queuelong_sphinx.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+model_name=swav_queuelong_dir
+base_dir="$model_name"_dir
+
+./dev/launch_slurm.sh \
+    $base_dir/trsnfeval/caltech \
+    config=benchmark/linear_image_classification/caltech101/eval_resnet_transfer_caltech_linear \
+    +config/server=sphinx1_4gpu \
+    config.DISTRIBUTED.NUM_NODES=1 \
+    config.DISTRIBUTED.NUM_PROC_PER_NODE=4 \
+    config.MODEL.WEIGHTS_INIT.PARAMS_FILE=$base_dir/checkpoints/model_final_checkpoint_phase399.torch \
+    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=196 \
+    config.DATA.NUM_DATALOADER_WORKERS=15 \
+    config.SLURM.PORT_ID=40093 \
+    config.SLURM.MEM_GB=300 \
+    config.SLURM.NUM_CPU_PER_PROC=16 \
