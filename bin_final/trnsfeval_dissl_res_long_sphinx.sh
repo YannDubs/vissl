8905eff98a98c3c5a9be42f3d3115c3969118f81
Yann Dubois <yanndubois96@gmail.com>
2022-08-02 19:06:15 -0700

    [ENH] add caltech

--- bin_final/trnsfeval_dissl_res_long_sphinx.sh
+++ bin_final/trnsfeval_dissl_res_long_sphinx.sh
@@ -5,11 +5,10 @@ base_dir="$model_name"_dir
 
 ./dev/launch_slurm.sh \
     $base_dir/trsnfeval \
-    config=benchmark/linear_image_classification/cifar10/eval_resnet_8gpu_transfer_cifar10_linear \
-    +config/server=sphinx2_4gpu \
+    config=benchmark/linear_image_classification/caltech101/eval_resnet_transfer_caltech_linear_z8 \
+    +config/server=sphinx1_4gpu \
     config.DISTRIBUTED.NUM_NODES=1 \
     config.DISTRIBUTED.NUM_PROC_PER_NODE=4 \
-    config.DATA.TRAIN.DATA_PATHS=["./data/imagenet/train"] \
     config.MODEL.WEIGHTS_INIT.PARAMS_FILE=$base_dir/checkpoints/model_final_checkpoint_phase399.torch \
     config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=196 \
     config.DATA.NUM_DATALOADER_WORKERS=15 \
