eda08770a7542e952a5ddc6380c21f5f92aa8297
Yann Dubois <yanndubois96@gmail.com>
2022-08-02 22:47:16 -0700

    [ENH] add food.

--- bin_final/trnsfeval_dissl_zdim8_long_sphinx.sh
+++ bin_final/trnsfeval_dissl_zdim8_long_sphinx.sh
@@ -3,29 +3,43 @@
 model_name=dissl_zdim8_long
 base_dir="$model_name"_dir
 
+#./dev/launch_slurm.sh \
+#    $base_dir/trsnfeval/caltech \
+#    config=benchmark/linear_image_classification/caltech101/eval_resnet_transfer_caltech_linear_z8 \
+#    +config/server=sphinx1_2gpu \
+#    config.DISTRIBUTED.NUM_NODES=1 \
+#    config.DISTRIBUTED.NUM_PROC_PER_NODE=2 \
+#    config.MODEL.WEIGHTS_INIT.PARAMS_FILE=$base_dir/checkpoints/model_final_checkpoint_phase399.torch \
+#    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=2048 \
+#    config.DATA.NUM_DATALOADER_WORKERS=31 \
+#    config.SLURM.PORT_ID=40080 \
+#    config.SLURM.MEM_GB=200 \
+#    config.SLURM.NUM_CPU_PER_PROC=32 \
+#
+#
+#./dev/launch_slurm.sh \
+#    $base_dir/trsnfeval/dtd \
+#    config=benchmark/linear_image_classification/dtd/eval_resnet_transfer_dtd_linear_z8 \
+#    +config/server=sphinx1_2gpu \
+#    config.DISTRIBUTED.NUM_NODES=1 \
+#    config.DISTRIBUTED.NUM_PROC_PER_NODE=2 \
+#    config.MODEL.WEIGHTS_INIT.PARAMS_FILE=$base_dir/checkpoints/model_final_checkpoint_phase399.torch \
+#    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=2048 \
+#    config.DATA.NUM_DATALOADER_WORKERS=31 \
+#    config.SLURM.PORT_ID=40081 \
+#    config.SLURM.MEM_GB=200 \
+#    config.SLURM.NUM_CPU_PER_PROC=32 \
+
+
 ./dev/launch_slurm.sh \
-    $base_dir/trsnfeval/caltech \
-    config=benchmark/linear_image_classification/caltech101/eval_resnet_transfer_caltech_linear_z8 \
+    $base_dir/trsnfeval/food101 \
+    config=benchmark/linear_image_classification/food101/eval_resnet_transfer_food_linear_z8 \
     +config/server=sphinx1_2gpu \
     config.DISTRIBUTED.NUM_NODES=1 \
     config.DISTRIBUTED.NUM_PROC_PER_NODE=2 \
     config.MODEL.WEIGHTS_INIT.PARAMS_FILE=$base_dir/checkpoints/model_final_checkpoint_phase399.torch \
     config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=2048 \
     config.DATA.NUM_DATALOADER_WORKERS=31 \
-    config.SLURM.PORT_ID=40093 \
-    config.SLURM.MEM_GB=200 \
-    config.SLURM.NUM_CPU_PER_PROC=32 \
-
-
-./dev/launch_slurm.sh \
-    $base_dir/trsnfeval/dtd \
-    config=benchmark/linear_image_classification/dtd/eval_resnet_transfer_dtd_linear_z8 \
-    +config/server=sphinx1_2gpu \
-    config.DISTRIBUTED.NUM_NODES=1 \
-    config.DISTRIBUTED.NUM_PROC_PER_NODE=2 \
-    config.MODEL.WEIGHTS_INIT.PARAMS_FILE=$base_dir/checkpoints/model_final_checkpoint_phase399.torch \
-    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=2048 \
-    config.DATA.NUM_DATALOADER_WORKERS=31 \
-    config.SLURM.PORT_ID=40094 \
+    config.SLURM.PORT_ID=40082 \
     config.SLURM.MEM_GB=200 \
     config.SLURM.NUM_CPU_PER_PROC=32 \
--- bin_final/trnsfeval_swav_queuelong_sphinx.sh
+++ bin_final/trnsfeval_swav_queuelong_sphinx.sh
@@ -3,29 +3,42 @@
 model_name=swav_queuelong
 base_dir="$model_name"_dir
 
+#./dev/launch_slurm.sh \
+#    $base_dir/trsnfeval/caltech \
+#    config=benchmark/linear_image_classification/caltech101/eval_resnet_transfer_caltech_linear \
+#    +config/server=sphinx1_2gpu \
+#    config.DISTRIBUTED.NUM_NODES=1 \
+#    config.DISTRIBUTED.NUM_PROC_PER_NODE=2 \
+#    config.MODEL.WEIGHTS_INIT.PARAMS_FILE=$base_dir/checkpoints/model_final_checkpoint_phase399.torch \
+#    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=2048 \
+#    config.DATA.NUM_DATALOADER_WORKERS=31 \
+#    config.SLURM.PORT_ID=40040 \
+#    config.SLURM.MEM_GB=200 \
+#    config.SLURM.NUM_CPU_PER_PROC=32 \
+#
+#
+#./dev/launch_slurm.sh \
+#    $base_dir/trsnfeval/dtd \
+#    config=benchmark/linear_image_classification/dtd/eval_resnet_transfer_dtd_linear \
+#    +config/server=sphinx1_2gpu \
+#    config.DISTRIBUTED.NUM_NODES=1 \
+#    config.DISTRIBUTED.NUM_PROC_PER_NODE=2 \
+#    config.MODEL.WEIGHTS_INIT.PARAMS_FILE=$base_dir/checkpoints/model_final_checkpoint_phase399.torch \
+#    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=2048 \
+#    config.DATA.NUM_DATALOADER_WORKERS=15 \
+#    config.SLURM.PORT_ID=40041 \
+#    config.SLURM.MEM_GB=200 \
+#    config.SLURM.NUM_CPU_PER_PROC=32 \
+
 ./dev/launch_slurm.sh \
-    $base_dir/trsnfeval/caltech \
-    config=benchmark/linear_image_classification/caltech101/eval_resnet_transfer_caltech_linear \
+    $base_dir/trsnfeval/food101 \
+    config=benchmark/linear_image_classification/food101/eval_resnet_transfer_food_linear \
     +config/server=sphinx1_2gpu \
     config.DISTRIBUTED.NUM_NODES=1 \
     config.DISTRIBUTED.NUM_PROC_PER_NODE=2 \
     config.MODEL.WEIGHTS_INIT.PARAMS_FILE=$base_dir/checkpoints/model_final_checkpoint_phase399.torch \
     config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=2048 \
     config.DATA.NUM_DATALOADER_WORKERS=31 \
-    config.SLURM.PORT_ID=40096 \
-    config.SLURM.MEM_GB=200 \
-    config.SLURM.NUM_CPU_PER_PROC=32 \
-
-
-./dev/launch_slurm.sh \
-    $base_dir/trsnfeval/dtd \
-    config=benchmark/linear_image_classification/dtd/eval_resnet_transfer_dtd_linear \
-    +config/server=sphinx1_2gpu \
-    config.DISTRIBUTED.NUM_NODES=1 \
-    config.DISTRIBUTED.NUM_PROC_PER_NODE=2 \
-    config.MODEL.WEIGHTS_INIT.PARAMS_FILE=$base_dir/checkpoints/model_final_checkpoint_phase399.torch \
-    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=2048 \
-    config.DATA.NUM_DATALOADER_WORKERS=15 \
-    config.SLURM.PORT_ID=40097 \
+    config.SLURM.PORT_ID=40042 \
     config.SLURM.MEM_GB=200 \
     config.SLURM.NUM_CPU_PER_PROC=32 \
