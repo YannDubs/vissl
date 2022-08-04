1f75d030718f6db0de4d493bf0e8283abe55887e
Yann Dubois <yanndubois96@gmail.com>
2022-08-04 14:13:26 -0700

    [ENH] add balanced metric

--- bin/collect_results_linear.py
+++ bin/collect_results_linear.py
@@ -7,11 +7,13 @@ DIR = Path(sys.argv[1])
 paths = list(DIR.glob("eval_*/model_*/**/train_size_-1/seed_0/all_metrics.csv"))
 paths += list(DIR.glob("eval/model_*/**/train_size_-1/seed_0/all_metrics.csv"))
 
+breakpoint()
 results = dict()
 for p in paths:
     eval_dir = str(p).split("eval_")[1].split("/")[0]
     results[eval_dir] = pd.read_csv(p, index_col=0)[[METRIC]].T
 
+breakpoint()
 all_results = pd.concat(results).droplevel(1)
 print(all_results)
 all_results.to_csv(DIR/"all_results.csv")
