2b361aafc6afa56135fc61853e1d3836d77f3751
Yann Dubois <yanndubois96@gmail.com>
2022-08-10 13:05:00 -0700

    run all

--- bin_final/dissl_zdim8_sphinx.sh
+++ bin_final/dissl_zdim8_sphinx.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-model=moco_nomulti
+model=dissl_zdim8
 
 ./dev/launch_slurm.sh \
     "$model"_dir \
-    config=pretrain/moco/"$model" \
-    +config/server=sphinx3 \
+    config=pretrain/dstl/"$model" \
+    +config/server=sphinx2_std \
     config.DISTRIBUTED.NUM_NODES=1 \
     config.DISTRIBUTED.NUM_PROC_PER_NODE=8 \
     config.DATA.TRAIN.DATA_PATHS=["./data/imagenet/train"] \
-    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=128 \
+    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=320 \
     config.DATA.NUM_DATALOADER_WORKERS=31 \
-    config.SLURM.PORT_ID=40020 \
+    config.SLURM.PORT_ID=40052 \
     config.SLURM.NAME="$model" \
     config.SLURM.MEM_GB=1000 \
     config.SLURM.NUM_CPU_PER_PROC=32 \
