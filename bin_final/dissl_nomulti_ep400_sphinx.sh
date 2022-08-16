248228e7dadd3f152ca1e62303e66ae465a20c16
Yann Dubois <yanndubois96@gmail.com>
2022-08-16 00:12:35 -0700

    sphinx 7-8

--- bin_final/dissl_nomulti_ep400_sphinx.sh
+++ bin_final/dissl_nomulti_ep400_sphinx.sh
@@ -5,7 +5,7 @@ model=dissl_nomulti_ep400
 ./dev/launch_slurm.sh \
     "$model"_dir \
     config=pretrain/dstl/"$model" \
-    +config/server=sphinx2_std \
+    +config/server=sphinx7 \
     config.DISTRIBUTED.NUM_NODES=1 \
     config.DISTRIBUTED.NUM_PROC_PER_NODE=8 \
     config.DATA.TRAIN.DATA_PATHS=["./data/imagenet/train"] \
--- bin_final/simclr_zdim8bnnoJL_nomulti_sphinx.sh
+++ bin_final/simclr_zdim8bnnoJL_nomulti_sphinx.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-model=dissl_ep400
+model=simclr_zdim8bnnoJL_nomulti
 
 ./dev/launch_slurm.sh \
     "$model"_dir \
-    config=pretrain/dstl/"$model" \
-    +config/server=sphinx8 \
+    config=pretrain/simclr/"$model" \
+    +config/server=sphinx2_std \
     config.DISTRIBUTED.NUM_NODES=1 \
     config.DISTRIBUTED.NUM_PROC_PER_NODE=8 \
     config.DATA.TRAIN.DATA_PATHS=["./data/imagenet/train"] \
-    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=320 \
+    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=384 \
     config.DATA.NUM_DATALOADER_WORKERS=31 \
-    config.SLURM.PORT_ID=40055 \
+    config.SLURM.PORT_ID=40012 \
     config.SLURM.NAME="$model" \
     config.SLURM.MEM_GB=1000 \
     config.SLURM.NUM_CPU_PER_PROC=32 \
