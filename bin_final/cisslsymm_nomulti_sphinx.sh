bae6186d92fa892bce500d5e4a64bb75dd7c8cb6
Yann Dubois <yanndubois96@gmail.com>
2022-08-18 09:50:33 -0700

    cissl sym

---
+++ bin_final/cisslsymm_nomulti_sphinx.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+model=cisslsymm_nomulti
+
+./dev/launch_slurm.sh \
+    "$model"_dir \
+    config=pretrain/cntr/"$model" \
+    +config/server=sphinx2_std \
+    config.DISTRIBUTED.NUM_NODES=1 \
+    config.DISTRIBUTED.NUM_PROC_PER_NODE=8 \
+    config.DATA.TRAIN.DATA_PATHS=["./data/imagenet/train"] \
+    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=384 \
+    config.DATA.NUM_DATALOADER_WORKERS=31 \
+    config.SLURM.PORT_ID=40014 \
+    config.SLURM.NAME="$model" \
+    config.SLURM.MEM_GB=1000 \
+    config.SLURM.NUM_CPU_PER_PROC=32 \
