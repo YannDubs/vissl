01aa0c141a7064a1b22281174025ce1fa0aaac9c
Yann Dubois <yanndubois96@gmail.com>
2022-08-18 11:56:19 -0700

    jagupard27 eval

--- bin/eval_nlprun.sh
+++ bin/eval_nlprun.sh
@@ -16,7 +16,7 @@ sbatch <<EOT
 #SBATCH --qos=normal
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
-#SBATCH --nodelist=jagupard26
+#SBATCH --nodelist=jagupard27
 #SBATCH --output="$dir"/eval_logs/slurm-%j.out
 #SBATCH --error="$dir"/eval_logs/slurm-%j.err
 
