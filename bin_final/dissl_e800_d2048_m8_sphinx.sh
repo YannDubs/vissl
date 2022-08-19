ca22713e51b467245704551899e08efcdb3b35c7
Yann Dubois <yanndubois96@gmail.com>
2022-08-19 03:18:25 -0700

    dissl_e800_d2048_m8

--- bin_final/dissl_e800_d2048_m8_sphinx.sh
+++ bin_final/dissl_e800_d2048_m8_sphinx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-model=dissl_e100_d8192_m2_mxml
+model=dissl_e800_d2048_m8
 
 ./dev/launch_slurm.sh \
     "$model"_dir \
@@ -11,7 +11,7 @@ model=dissl_e100_d8192_m2_mxml
     config.DATA.TRAIN.DATA_PATHS=["./data/imagenet/train"] \
     config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=320 \
     config.DATA.NUM_DATALOADER_WORKERS=31 \
-    config.SLURM.PORT_ID=40033 \
+    config.SLURM.PORT_ID=40069 \
     config.SLURM.NAME="$model" \
     config.SLURM.MEM_GB=1000 \
     config.SLURM.NUM_CPU_PER_PROC=32 \
