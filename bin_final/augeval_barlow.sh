aac6655b67cf9f83e576d3456d200e9595d93553
Yann Dubois <yanndubois96@gmail.com>
2022-08-26 14:32:31 -0700

    augeval_barlow

---
+++ bin_final/augeval_barlow.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+base_dir="$1"
+epochs="$2"
+
+
+./dev/launch_slurm.sh \
+    $base_dir/augeval_barlow \
+    config=benchmark/linear_image_classification/imagenet1k/eval_resnet_in1k_linear_barlow \
+    +config/server=sphinx \
+    config.DISTRIBUTED.NUM_NODES=1 \
+    config.DISTRIBUTED.NUM_PROC_PER_NODE=1 \
+    config.DATA.TRAIN.DATA_PATHS=["./data/imagenet/train"] \
+    config.DATA.TEST.DATA_PATHS=["./data/imagenet/train"] \
+    config.MODEL.WEIGHTS_INIT.PARAMS_FILE=$base_dir/checkpoints/model_final_checkpoint_phase"$epochs".torch \
+    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=2048 \
+    config.DATA.TEST.BATCHSIZE_PER_REPLICA=4096 \
+    config.DATA.NUM_DATALOADER_WORKERS=31 \
+    config.SLURM.PORT_ID=40066 \
+    config.SLURM.NAME=augevalsw_"$base_dir" \
+    config.SLURM.MEM_GB=256 \
+    config.SLURM.NUM_CPU_PER_PROC=32 \
