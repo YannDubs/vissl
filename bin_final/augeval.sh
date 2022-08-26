287f3fe32105994ecb6d0966ce3aacb6ad2deb49
Yann Dubois <yanndubois96@gmail.com>
2022-08-26 11:16:02 -0700

    augeval single GPU

--- bin_final/augeval.sh
+++ bin_final/augeval.sh
@@ -7,15 +7,15 @@ epochs="799"
 ./dev/launch_slurm.sh \
     $base_dir/augeval \
     config=benchmark/linear_image_classification/imagenet1k/eval_resnet_in1k_linear \
-    +config/server=sphinx2_std \
+    +config/server=sphinx2 \
     config.DISTRIBUTED.NUM_NODES=1 \
-    config.DISTRIBUTED.NUM_PROC_PER_NODE=8 \
+    config.DISTRIBUTED.NUM_PROC_PER_NODE=1 \
     config.DATA.TRAIN.DATA_PATHS=["./data/imagenet/train"] \
     config.DATA.TEST.DATA_PATHS=["./data/imagenet/train"] \
     config.MODEL.WEIGHTS_INIT.PARAMS_FILE=$base_dir/checkpoints/model_final_checkpoint_phase"$epochs".torch \
-    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=480 \
+    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=1024 \
     config.DATA.NUM_DATALOADER_WORKERS=31 \
     config.SLURM.PORT_ID=40061 \
     config.SLURM.NAME=augeval_"$base_dir" \
-    config.SLURM.MEM_GB=1000 \
+    config.SLURM.MEM_GB=128 \
     config.SLURM.NUM_CPU_PER_PROC=32 \
