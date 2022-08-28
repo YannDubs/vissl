2b7f813efc2fbb6177e4632296221e09926ffd90
Yann Dubois <yanndubois96@gmail.com>
2022-08-27 17:46:39 -0700

    add all simclr heads

---
+++ bin_final/simclr_sym_sphinx.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+model=simclr_sym
+
+./dev/launch_slurm.sh \
+    "$model"_dir \
+    config=pretrain/simclr/"$model" \
+    +config/server=sphinx4 \
+    config.DISTRIBUTED.NUM_NODES=1 \
+    config.DISTRIBUTED.NUM_PROC_PER_NODE=8 \
+    config.DATA.TRAIN.DATA_PATHS=["./data/imagenet/train"] \
+    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=384 \
+    config.DATA.NUM_DATALOADER_WORKERS=31 \
+    config.SLURM.PORT_ID=40023 \
+    config.SLURM.NAME="$model" \
+    config.SLURM.MEM_GB=1000 \
+    config.SLURM.NUM_CPU_PER_PROC=32 \
