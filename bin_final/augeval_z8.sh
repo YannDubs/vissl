ae81dc6492e1445785eab1b26217c5f405407ce5
Yann Dubois <yanndubois96@gmail.com>
2022-08-30 14:04:33 -0700

    run all augeval

--- bin_final/augeval_z8.sh
+++ bin_final/augeval_z8.sh
@@ -7,7 +7,7 @@ epochs="$2"
 ./dev/launch_slurm.sh \
     $base_dir/augeval \
     config=benchmark/linear_image_classification/imagenet1k/eval_resnet_in1k_linear_z8 \
-    +config/server=sphinx7_1 \
+    +config/server=sphinx4_1 \
     config.DISTRIBUTED.NUM_NODES=1 \
     config.DISTRIBUTED.NUM_PROC_PER_NODE=1 \
     config.DATA.TRAIN.DATA_PATHS=["./data/imagenet/train"] \
