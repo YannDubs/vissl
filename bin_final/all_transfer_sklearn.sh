43a66798387e6d6653c0970f16bbab5083ad66a2
Yann Dubois <yanndubois96@gmail.com>
2022-08-30 15:06:05 -0700

    trnsf eval

--- bin_final/all_transfer_sklearn.sh
+++ bin_final/all_transfer_sklearn.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-for DATA in flowers pets caltech cars aircrafts dtd food101 #sun397 cifar10 cifar100
+for DATA in flowers pets caltech cars aircrafts dtd food101  cifar10 cifar100 #sun397
 do
-    bin/trsnfeval_sklearn_nlprun.sh dissl_zdim8_long_dir _z8192 $DATA
+    bin/trsnfeval_sklearn_nlprun.sh dissl_e400_d8192_m6_dir _z8192 $DATA
     #bin/trsnfeval_sklearn_nlprun.sh swav_queuelong_dir "" $DATA
     #bin/trsnfeval_sklearn_nlprun.sh simclr_nomulti_long_dir "" $DATA
 
